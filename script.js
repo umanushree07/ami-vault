3f1332e6b7c3bd74ba00dcdd86b44aa66d262118
umanushree07 <umanushree07@gmail.com>
2026-01-02 13:58:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -88,13 +88,3 @@ function watchAdForDownload(button) {
 
   }, 5000);
 }
-
-function openSidebar() {
-  document.getElementById("sidebar").classList.add("open");
-  document.getElementById("overlay").style.display = "block";
-}
-
-function closeSidebar() {
-  document.getElementById("sidebar").classList.remove("open");
-  document.getElementById("overlay").style.display = "none";
-}
